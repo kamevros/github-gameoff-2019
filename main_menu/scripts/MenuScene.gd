2779193f7f76eb7527637bd8375b7340c5a6cb50
Karbb <carbchannel@gmail.com>
2019-11-12 00:26:29 +0100

    main menu choice and rafting scene

--- main_menu/scripts/MenuScene.gd
+++ main_menu/scripts/MenuScene.gd
@@ -1,8 +1,48 @@
 extends Node
 
-var next_scene = "res://climbing_scene/scenes/ClimbingScene.tscn"
+var climbing_scene = "res://climbing_scene/scenes/ClimbingScene.tscn"
+var rafting_scene = "res://rafting_scene/scenes/RaftingScene.tscn"
+var hiking_scene = "res://hiking_scene/scenes/HikingScene.tscn"
 
+export(int, 0, 2) var selector : int = 0
+
+onready var beatrix_sprite : AnimatedSprite = $CanvasLayer/Characters/Beatrix as AnimatedSprite
+onready var gustaf_sprite : AnimatedSprite = $CanvasLayer/Characters/Gustaf as AnimatedSprite
+onready var remy_sprite : AnimatedSprite = $CanvasLayer/Characters/Remy as AnimatedSprite
+onready var arrow : Sprite = $CanvasLayer/Characters/Arrow as Sprite
+onready var selector_array = [remy_sprite, beatrix_sprite, gustaf_sprite]
+onready var scene_array = [rafting_scene, hiking_scene, climbing_scene]
+
+var second_counter : float = 0
+
+func _ready() -> void:
+	for sprite in selector_array:
+		sprite.animation = "default"
 
 func _process(delta : float) -> void:
+	second_counter += delta
+	var uniform_periodic = abs(cos(second_counter*3))
+
+	selector_array[selector].material.set_shader_param("alpha", uniform_periodic)
+	
+	for sprite in selector_array:
+		if sprite == selector_array[selector]:
+			sprite.animation = "idle"
+			arrow.position.y = sprite.position.y - 50
+			arrow.position.x = sprite.position.x
+			continue
+		sprite.material.set_shader_param("alpha", 0)
+		sprite.animation = "default"
+	
 	if Input.is_action_pressed("ui_accept"):
-		scene_manager.change_scene(next_scene)
+		scene_manager.change_scene(scene_array[selector])
+		
+	if Input.is_action_just_pressed("movement_left"):
+		selector -= 1
+		if selector < 0:
+			selector = selector_array.size() - 1
+		
+	elif Input.is_action_just_pressed("movement_right"):
+		selector += 1
+		if selector > selector_array.size() - 1:
+			selector = 0
--- rafting_scene/scripts/Remy.gd
+++ rafting_scene/scripts/Remy.gd
@@ -1,33 +1,83 @@
 extends KinematicBody2D
+class_name PlayerMouse
+
+export(int, 50, 150) var velocity_y = 100
+export(int, 50, 150) var offset_y = 75
 
 onready var sprite : AnimatedSprite = $AnimatedSprite as AnimatedSprite
 onready var camera = get_node("../Camera2D")
+onready var horizontal_movement_tween : Tween = $HorizontalMovementTween
+onready var vertical_movement_tween : Tween= $VerticalMovementTween
 
 var direction : = Vector2()
 var velocity : = Vector2()
+
+var tween_active : bool = false
 
 func _ready() -> void:
 	sprite.play("idle")
 
 
 func _process(delta : float) -> void:
-	camera.position.y = position.y - 50
-	direction = get_input()
-
+	camera.position.y = position.y - offset_y
+	direction = get_input(delta)
+	
 
 func _physics_process(delta : float) -> void:
-	velocity.y = -100
+	velocity.y = - velocity_y
 	velocity = move_and_slide(velocity)
+	
+	
+func movement(direction : Vector2):
+	if tween_active:
+		return
+		
+	if direction.x != 0:
+		horizontal_movement_tween.interpolate_property(self, "position:x",
+	        position.x, position.x + 25 * direction.x, .5,
+	        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween_active = true
+		
+		horizontal_movement_tween.start()
+		
+	elif direction.y != 0:
+		vertical_movement_tween.interpolate_property(self, "offset_y",
+        offset_y, offset_y + 25 * direction.y, .5,
+        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween_active = true
+		
+		vertical_movement_tween.start()
+	
 
-
-func get_input() -> Vector2:
+func get_input(delta : float) -> Vector2:
 	var direction : = Vector2()
 
-	if Input.is_action_pressed("movement_right"):
-		direction.x = +1
-		sprite.flip_h = false
-	elif Input.is_action_pressed("movement_left"):
+	if Input.is_action_just_pressed("movement_right"):
 		direction.x = -1
+		sprite.flip_h = false
+		movement(direction)
+		
+	elif Input.is_action_just_pressed("movement_left"):
+		direction.x = +1
 		sprite.flip_h = true
-			
+		movement(direction)
+		
+	elif Input.is_action_just_pressed("movement_up"):
+		velocity_y = min(velocity_y + 25, 150)
+		direction.y = +1
+		movement(direction)
+		
+	elif Input.is_action_just_pressed("movement_down"):
+		velocity_y = max(velocity_y - 25, 50)
+		direction.y = -1
+		movement(direction)
+	
 	return direction
+	
+
+func _on_HorizontalMovementTween_tween_all_completed():
+	tween_active = false
+
+
+func _on_VerticalMovementTween_tween_all_completed():
+	tween_active = false
