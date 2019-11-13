8a97ef4c896b37ec612861046bb350fd9f444811
Karbb <carbchannel@gmail.com>
2019-11-13 16:28:48 +0100

    quit scene

--- main_menu/scripts/MenuScene.gd
+++ main_menu/scripts/MenuScene.gd
@@ -4,6 +4,7 @@ var climbing_scene = "res://climbing_scene/scenes/ClimbingScene.tscn"
 var rafting_scene = "res://rafting_scene/scenes/RaftingScene.tscn"
 var hiking_scene = "res://hiking_scene/scenes/HikingScene.tscn"
 var option_scene = "res://option_scene/scenes/OptionScene.tscn"
+var quit_scene = "res://quit_scene/scenes/QuitScene.tscn"
 
 export(int, 0, 2) var selector : int = 0
 
@@ -11,8 +12,10 @@ onready var beatrix_sprite : AnimatedSprite = $Characters/Beatrix as AnimatedSpr
 onready var gustaf_sprite : AnimatedSprite = $Characters/Gustaf as AnimatedSprite
 onready var remy_sprite : AnimatedSprite = $Characters/Remy as AnimatedSprite
 onready var tent_sprite : AnimatedSprite = $Background/Tent as AnimatedSprite
-onready var selector_array = [remy_sprite, beatrix_sprite, gustaf_sprite, tent_sprite]
-onready var scene_array = [rafting_scene, hiking_scene, climbing_scene, option_scene]
+onready var sign_sprite : AnimatedSprite = $Background/Sign as AnimatedSprite
+
+onready var selector_array = [sign_sprite, remy_sprite, beatrix_sprite, gustaf_sprite, tent_sprite]
+onready var scene_array = [quit_scene, rafting_scene, hiking_scene, climbing_scene, option_scene]
 
 var second_counter : float = 0
 
@@ -45,3 +48,4 @@ func _process(delta : float) -> void:
 		selector += 1
 		if selector > selector_array.size() - 1:
 			selector = 0
+			
